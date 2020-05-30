2d77196cd827edc51d7b79e5a13bc5fa3b1698df
willian emboava <w.emboava.o@gmail.com>
2020-05-29 22:01:11 -0300

    create user

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -5,12 +5,12 @@ import AppointmentsRepository from "../repositories/AppointmentsRepository";
 import Appointment from "../models/Appointment";
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ provider, date }: Request): Promise<Appointment> {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
     const appointmentDate = startOfHour(date);
@@ -24,7 +24,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider_id: provider,
+      provider_id,
       date: appointmentDate,
     });
 
