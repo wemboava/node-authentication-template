4a374c5abe6be9cbfb54b7c76f813d1bcbf6bd87
willian emboava <w.emboava.o@gmail.com>
2020-05-31 11:21:06 -0300

    Implemented avatar image upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
+
+import uploadConfig from "../config/upload";
 
 import CreateUserService from "../services/CreateUserService";
 
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post("/", async (request, response) => {
   try {
@@ -17,5 +23,14 @@ usersRouter.post("/", async (request, response) => {
     return response.status(400).json({ message: err.message });
   }
 });
+
+usersRouter.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  async (request, response) => {
+    return response.send();
+  }
+);
 
 export default usersRouter;
