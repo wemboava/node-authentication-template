db27c9e654a67a4a7f39ef4a2285732f184b63fd
willian emboava <w.emboava.o@gmail.com>
2020-07-28 21:23:27 -0300

    code refactored

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,9 +1,9 @@
 import { startOfHour } from "date-fns";
-import { getCustomRepository } from "typeorm";
 
 import AppError from "@shared/errors/AppError";
-import AppointmentsRepository from "../repositories/AppointmentsRepository";
 import Appointment from "../infra/typeorm/entities/Appointment";
+
+import IAppointmentsRepository from "../repositories/IAppointmentsRepository";
 
 interface Request {
   provider_id: string;
@@ -11,12 +11,12 @@ interface Request {
 }
 
 class CreateAppointmentService {
-  public async execute({ provider_id, date }: Request): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  constructor(private appointmentsRepository: IAppointmentsRepository) {}
 
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate
     );
 
@@ -24,12 +24,10 @@ class CreateAppointmentService {
       throw new AppError("This appointment is already booked!");
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = await this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
-
-    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
