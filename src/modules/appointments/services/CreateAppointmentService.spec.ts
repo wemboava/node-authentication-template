cf231474c522d1e56b57a05fdf3c693f6fb8f9a5
willian emboava <w.emboava.o@gmail.com>
2020-08-01 19:25:54 -0300

    created fakeAppointmentRepository

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,23 @@
-test("sun two numbers", () => {
-  expect(2 + 2).toBe(4);
+import CreateAppointmentService from "./CreateAppointmentService";
+import FakeAppointmentsRepository from "../repositories/fakes/FakeAppointmentsRepository";
+
+describe("CreateAppointment", () => {
+  it("should be able to create a new appointment", async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: "12345",
+    });
+
+    expect(appointment).toHaveProperty("id");
+    expect(appointment.provider_id).toBe("12345");
+  });
+
+  // it("should not be able to create two appointment on the same time", () => {
+
+  // });
 });
