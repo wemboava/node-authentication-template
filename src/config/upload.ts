558c1502668a88e81aadc8bbf45f5426e67a9db7
willian emboava <w.emboava.o@gmail.com>
2020-08-04 15:45:09 -0300

    bug fix :: tmpFolder

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -9,7 +9,7 @@ export default {
   uploadsFolder: path.resolve(tmpFolder, "uploads"),
 
   storage: multer.diskStorage({
-    destination: tempFolder,
+    destination: tmpFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString("hex");
       const fileName = `${fileHash}-${file.originalname}`;
